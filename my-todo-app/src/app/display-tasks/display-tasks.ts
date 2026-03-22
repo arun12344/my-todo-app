1dc0e3486869f03a72833238c103a177f12bfac9
unknown <arun.ravisankkar@yahoo.com>
2026-03-22 21:58:16 +0530

    Status colour and id is added

--- my-todo-app/src/app/display-tasks/display-tasks.ts
+++ my-todo-app/src/app/display-tasks/display-tasks.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Task } from './display-tasks.model';
 import { MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-display-tasks',
-  imports: [MatCardTitle,MatCardSubtitle,MatCardHeader,MatCardContent,MatCard],
+  imports: [MatCardTitle,MatCardSubtitle,MatCardHeader,MatCardContent,MatCard,NgClass],
   templateUrl: './display-tasks.html',
   styleUrl: './display-tasks.css',
 })
